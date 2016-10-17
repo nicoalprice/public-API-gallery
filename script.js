ff4d03769000ef632c062a63235f3485f245eee4
Nicoal Price <nicoalprice@gmail.com>
2016-10-17 17:18:17 -0400

    created new repository

---
+++ script.js
@@ -0,0 +1,17 @@
+$document.ready(function() {
+
+	//When search button is submitted...
+	$('form').submit( function() {
+
+
+	var openLibraryAPI = "https://openlibrary.org/api/books";
+	var options;
+	function displayBooks() {
+
+	};
+
+	$.getJSON(openLibraryAPI, options, displayBooks;
+	}); //end submit function
+
+
+}); //end
