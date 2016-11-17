f8dd3370e69c87c2b0a2cf5f77e49c01ac8726aa
Nicoal Price <nicoalprice@gmail.com>
2016-11-17 11:34:16 -0500

    adding JSON request/response info

--- js/script.js
+++ js/script.js
@@ -1,17 +1,35 @@
 $document.ready(function() {
 
+	// User enters a search term
+	// Receive JSON response
+	// Add a link and <img> tag to page
+
 	//When search button is submitted...
 	$('form').submit( function() {
-
-
-	var openLibraryAPI = "https://openlibrary.org/api/books";
+	// Javascript retrieves that search term
+	var userSearch = //form input
+	var openLibraryAPI = 'http://openlibrary.org/search.json?q=';
+		openLibraryAPI += 'userSearch';
 	var options;
-	function displayBooks() {
-
+	function displayBooks(data) {
+		var bookHTML = ''<ul id="gallery">';
+		$.each(data.items, function(i, book) {
+			bookHTML += '<li>';
+			bookHTML += '<a href="https://openlibrary.org/api/books?bibkeys=OLID:';
+			bookHTML += 'book.docs.key"';
+			bookHTML +='</a><img src="book.docs.cover_i"/>';
+			bookHTML +='</li>';
+		});
+		bookHTML += '</ul>';
 	};
-
+	// Make a GET request to OpenLibrary, sending the search term along
 	$.getJSON(openLibraryAPI, options, displayBooks;
 	}); //end submit function
 
 
-}); //end
+}); //end ready
+
+	/*
+
+	put key from search after colon
+	&bibkeys=OLID:OL123M
