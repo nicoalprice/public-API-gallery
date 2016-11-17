ff33911b720cacf9ec9f491b9f18b4bbafa68eb5
Nicoal Price <nicoalprice@gmail.com>
2016-11-17 14:09:14 -0500

    getting search box input value

--- js/script.js
+++ js/script.js
@@ -1,35 +1,40 @@
-$document.ready(function() {
+//$document.ready(function() {
 
 	// User enters a search term
 	// Receive JSON response
 	// Add a link and <img> tag to page
 
-	//When search button is submitted...
-	$('form').submit( function() {
+	//When user hits enter key...
+	$('#search').keypress(function(e) {
+		if (e.which == 13) { // enter key
+			e.preventDefault;
+
 	// Javascript retrieves that search term
-	var userSearch = //form input
-	var openLibraryAPI = 'http://openlibrary.org/search.json?q=';
-		openLibraryAPI += 'userSearch';
-	var options;
-	function displayBooks(data) {
-		var bookHTML = ''<ul id="gallery">';
-		$.each(data.items, function(i, book) {
-			bookHTML += '<li>';
-			bookHTML += '<a href="https://openlibrary.org/api/books?bibkeys=OLID:';
-			bookHTML += 'book.docs.key"';
-			bookHTML +='</a><img src="book.docs.cover_i"/>';
-			bookHTML +='</li>';
-		});
-		bookHTML += '</ul>';
-	};
-	// Make a GET request to OpenLibrary, sending the search term along
-	$.getJSON(openLibraryAPI, options, displayBooks;
-	}); //end submit function
+		var userSearch = $('input#search').val();
+		alert(userSearch);
+		}
+	});
+
+//	var openLibraryAPI = 'http://openlibrary.org/search.json?q=';
+//		openLibraryAPI += 'userSearch';
+//	var options;
+//	function displayBooks(data) {
+//		var bookHTML = ''<ul id="gallery">';
+//		$.each(data.items, function(i, book) {
+//			bookHTML += '<li>';
+//			bookHTML += '<a href="https://openlibrary.org/api/books?bibkeys=OLID:';
+//			bookHTML += 'book.docs.key"';
+//			bookHTML +='</a><img src="book.docs.cover_i"/>';
+//			bookHTML +='</li>';
+//		});
+//		bookHTML += '</ul>';
+//	};
+//	// Make a GET request to OpenLibrary, sending the search term along
+//	$.getJSON(openLibraryAPI, options, displayBooks;
+//	}); //end submit function
 
 
-}); //end ready
+//}); //end ready
 
-	/*
-
-	put key from search after colon
-	&bibkeys=OLID:OL123M
+//	put key from search after colon
+//	&bibkeys=OLID:OL123M
