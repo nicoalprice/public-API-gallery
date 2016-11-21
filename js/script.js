2808f4cae94f1feea2b8e824bcfaf6513fa730fa
Nicoal Price <nicoalprice@gmail.com>
2016-11-21 15:34:15 -0500

    working on json request

--- js/script.js
+++ js/script.js
@@ -11,9 +11,8 @@ $(document).ready(function() {
 			alert(userSearch); // test
 
 			// Construct search url
-			var openLibraryAPI = 'https://openlibrary.org/search.json?';
+			var openLibraryAPI = 'http//openlibrary.org/search.json?';
 				openLibraryAPI += userSearch;
-				openLibraryAPI += "&callback=?";
 			alert(openLibraryAPI); // test
 
 			// Get records for first 10 results and display covers
@@ -25,9 +24,9 @@ $(document).ready(function() {
 				for (i=0; i<10; i++) {
 					$.each(bookData.items, function(i, book) {
 						bookHTML += '<li>';
-						bookHTML += '<a href="https://openlibrary.org/api/books?bibkeys=OLID:';
+						bookHTML += '<a href="http://openlibrary.org/api/books?bibkeys=OLID:';
 						bookHTML += book.docs[i].edition_key + '"</a>';
-						bookHTML +='<img src="p://covers.openlibrary.org/b/OLID/"';
+						bookHTML +='<img src="http://covers.openlibrary.org/b/OLID/"';
 						bookHTML += book.docs[i].edition_key;
 						bookHTML += '/-M.jpg" />';
 						bookHTML +='</li>';
