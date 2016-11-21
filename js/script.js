608e4516ff520587d5402b1894f9687efdd0d48a
Nicoal Price <nicoalprice@gmail.com>
2016-11-21 15:19:24 -0500

    cleaned up ajax request js

--- js/script.js
+++ js/script.js
@@ -6,45 +6,46 @@ $(document).ready(function() {
 		if (e.which == 13) { // enter
 			e.preventDefault;
 
-	// Get search term
-		var userSearch = $('input#search').serialize();
-		alert(userSearch);
+			// Get search term
+			var userSearch = $('input#search').serialize();
+			alert(userSearch); // test
 
-	// Construct search url
-		var openLibraryAPI = 'http://openlibrary.org/search.json?';
-		openLibraryAPI += userSearch;
-		alert(openLibraryAPI);
+			// Construct search url
+			var openLibraryAPI = 'https://openlibrary.org/search.json?';
+				openLibraryAPI += userSearch;
+				openLibraryAPI += "&callback=?";
+			alert(openLibraryAPI); // test
 
-	// Get records for first 10 results and display covers
-		var options;
+			// Get records for first 10 results and display covers
+			var options;
 
-		function displayBooks(bookData) {
-			var bookHTML = '<ul>';
-			for (i=0; i<10; i++) {
-//			$.each(data.items, function(i, book) {
-				bookHTML += '<li>';
-				bookHTML += '<a href="https://openlibrary.org/api/books?bibkeys=OLID:';
-				bookHTML += book[i].docs.edition_key + '"</a>';
-				bookHTML +='<img src="p://covers.openlibrary.org/b/OLID/"';
-				bookHTML += book[i].docs.edition_key;
-				bookHTML += '/-M.jpg" />';
-				bookHTML +='</li>';
-
-			} // end loop
-
-			bookHTML += '</ul>';
-			console.log(bookHTML); //
-
-			$('#gallery').html(bookHTML);
-		} // end displayBooks
+			function displayBooks(bookData) {
+				var bookHTML = '<ul>';
+				// Loop through search results
+				for (i=0; i<10; i++) {
+					$.each(bookData.items, function(i, book) {
+						bookHTML += '<li>';
+						bookHTML += '<a href="https://openlibrary.org/api/books?bibkeys=OLID:';
+						bookHTML += book.docs[i].edition_key + '"</a>';
+						bookHTML +='<img src="p://covers.openlibrary.org/b/OLID/"';
+						bookHTML += book.docs[i].edition_key;
+						bookHTML += '/-M.jpg" />';
+						bookHTML +='</li>';
+					}); // end anonymous function
+				}; // end for loop
 
 
+				bookHTML += '</ul>';
+				console.log(bookHTML); // test
+
+				$('#gallery').html(bookHTML);
+			}; // end displayBooks
 
 
-		// Make a GET request to OpenLibrary, sending the search term along
-		$.getJSON(openLibraryAPI, options, displayBooks);
+			// Make a GET request to OpenLibrary, sending the search term along
+			$.getJSON(openLibraryAPI, options, displayBooks);
 
-					} // end if statement
+		} // end if statement
 
 	}); //end submit function
 
