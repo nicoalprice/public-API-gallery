63aa452cc59d86839c9383db9448d22639db99f7
Nicoal Price <nicoalprice@gmail.com>
2016-11-23 13:10:07 -0500

    added titles to gallery

--- js/script.js
+++ js/script.js
@@ -11,21 +11,21 @@ $(document).ready(function() {
 
 			// Get search term
 			var userSearch = $('input#search').serialize();
-			alert(userSearch); // test
 
 			// Callback function to get records for first 10 results and display covers
 			function displayBooks(response) {
 				console.log(response);
 				var bookHTML = '<ul>'; // start gallery list
 				// Loop through search results
-				for (i=0; i<10; i++) {
+				for (i=0; i<25; i++) {
 //					$.each(bookData.docs, function(i, book) {
 						bookHTML += '<li>';
 						bookHTML += '<a href="http://openlibrary.org/books/';
 						bookHTML += response.docs[i].edition_key[0] + '" target="_blank">';
 						bookHTML +='<img src="http://covers.openlibrary.org/b/olid/';
-						bookHTML += response.docs[i].edition_key[0];
-						bookHTML += '-M.jpg" /></a>';
+						bookHTML += response.docs[i].cover_edition_key;
+						bookHTML += '-M.jpg" alt="' + response.docs[i].title + '"/></a>';
+						bookHTML += '<p class="book-title">' + response.docs[i].title + '</p>'
 						bookHTML +='</li>';
 //					}; // end anonymous function
 				}; // end for loop
