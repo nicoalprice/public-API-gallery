c5550627bc5ea82c097a4e4c14829669622cb07e
Nicoal Price <nicoalprice@gmail.com>
2016-11-25 14:37:28 -0500

    added movie api and radio buttons

--- js/script.js
+++ js/script.js
@@ -6,84 +6,94 @@ $(document).ready(function() {
 			e.preventDefault();
 
 			// If book search is selected
-			// Book search url
-			var openLibraryAPI = 'https://openlibrary.org/search.json';
-			// Get search term
-			var userSearch = $('input#search').serialize();
+			if ($('#book-search').attr("checked") == "checked") {
 
-			// Callback function to display book covers
-			function displayBooks(response) {
-				console.log(response);
-				var bookHTML = '<ul>'; // start gallery list
-				var books = response.docs;
+				// Book search url
+				var openLibraryAPI = 'https://openlibrary.org/search.json';
+				// Get search term
+				var userSearch = $('input#search').serialize();
 
-				if (response.numFound == 0) {
-						bookHTML += '<p id="no-results">No results found.</p>';
-					}
+				// Callback function to display book covers
+				function displayBooks(response) {
+					console.log(response);
+					var bookHTML = '<ul>'; // start gallery list
+					var books = response.docs;
 
-				else {
-				// Loop through search results
-					for (i=0; i<100; i++) {
-						// Only show titles with cover images
-						if (books[i].cover_edition_key != undefined) {
-							bookHTML += '<li>';
-							bookHTML += '<a href="http://openlibrary.org/books/';
-							bookHTML += books[i].edition_key[0] + '" target="_blank">';
-							bookHTML +='<img src="http://covers.openlibrary.org/b/olid/';
-							bookHTML += books[i].cover_edition_key;
-							bookHTML += '-M.jpg" alt="' + books[i].title + '"/></a>';
-							bookHTML += '<p class="book-title">' + books[i].title + '</p>';
-							bookHTML +='</li>';
+					if (response.numFound == 0) {
+							bookHTML += '<p id="no-results">No results found.</p>';
 						}
-	//					}; // end anonymous function
-					}; // end for loop
-				} // end else statement
 
-				bookHTML += '</ul>'; // end gallery list
+					else {
+					// Loop through search results
+						for (i=0; i<100; i++) {
+							// Only show titles with cover images
+							if (books[i].cover_edition_key != undefined) {
+								bookHTML += '<li>';
+								bookHTML += '<a href="http://openlibrary.org/books/';
+								bookHTML += books[i].edition_key[0] + '" target="_blank">';
+								bookHTML +='<img src="http://covers.openlibrary.org/b/olid/';
+								bookHTML += books[i].cover_edition_key;
+								bookHTML += '-M.jpg" alt="' + books[i].title + '"/></a>';
+								bookHTML += '<p class="book-title">' + books[i].title + '</p>';
+								bookHTML +='</li>';
+							}
+		//					}; // end anonymous function
+						}; // end for loop
+					} // end else statement
 
-				$('#gallery').html(bookHTML);
-			}; // end displayBooks
+					bookHTML += '</ul>'; // end gallery list
+
+					$('#gallery').html(bookHTML);
+				}; // end displayBooks
+
+				// Make a GET request to OpenLibrary, sending the search term along
+				$.getJSON(openLibraryAPI, userSearch, displayBooks);
+
+			} // end if book checked statement
 
 
 			// If movie search is selected
+			if ($('#movie-search').val() == "movie-search") {
+
 			// Movie search url
-			var movieAPI = 'https://www.omdbapi.com/?';
-			// Get search term
-			var movieSearch = 's=' + $('input#search');
+				var movieAPI = 'https://www.omdbapi.com/?';
+				// Get search term
+				var movieSearch = 's=' + $('input#search');
 
-			// Callback function to display movie posters
-			function displayMovies(response) {
-				console.log(response);
-				var movies = response.Search;
-				var movieHTML = '<ul>'; // start gallery list
+				// Callback function to display movie posters
+				function displayMovies(response) {
+					console.log(response);
+					var movies = response.Search;
+					var movieHTML = '<ul>'; // start gallery list
 
-				if (response.Response == false) {
-						movieHTML += '<p id="no-results">No results found.</p>';
-					}
+					if (response.Response == false) {
+							movieHTML += '<p id="no-results">No results found.</p>';
+						}
 
-				else {
-				// Loop through search results
+					else {
+					// Loop through search results
 
-					for (i=0; i<movies.length; i++) {
-							movieHTML += '<li>';
-							movieHTML +='<img src="';
-							movieHTML += movies[i].Poster + '" ';
-							movieHTML += 'alt="' + movies[i].Title + '"/></a>';
-							movieHTML += '<p class="movie-title">' + movies[i].Title + '</p>';
-							movieHTML +='</li>';
-						} // end for loop
-				} // end else statement
+						for (i=0; i<movies.length; i++) {
+							if (movies[i].Poster != "N/A") {
+								movieHTML += '<li>';
+								movieHTML +='<img src="';
+								movieHTML += movies[i].Poster + '" ';
+								movieHTML += 'alt="' + movies[i].Title + '"/></a>';
+								movieHTML += '<p class="movie-title">' + movies[i].Title + '</p>';
+								movieHTML +='</li>';
+							}} // end for loop
+					} // end else statement
 
-				movieHTML += '</ul>'; // end gallery list
+					movieHTML += '</ul>'; // end gallery list
 
-				$('#gallery').html(movieHTML);
-			}; // end displayMovies
+					$('#gallery').html(movieHTML);
+				}; // end displayMovies
 
 
-			// Make a GET request to OpenLibrary, sending the search term along
-			$.getJSON(openLibraryAPI, userSearch, displayBooks);
-			// AJAX request to OMDB
-			$.getJSON(movieAPI, movieSearch, displayMovies);
+				// AJAX request to OMDB
+				$.getJSON(movieAPI, movieSearch, displayMovies);
+
+			} // end if movie search checked statement
 
 		} // end if statement for true/false response
 
