b3095e81e024ed2aaf2be276be6bfb543dad9e11
Nicoal Price <nicoalprice@gmail.com>
2016-11-30 12:20:58 -0500

    still working on showing plot

---
+++ js/overlay.js
@@ -0,0 +1,156 @@
+
+/*** OVERLAY ***/
+
+	var $overlay = $('<div id="overlay"></div>');
+	var $image = $('<img id="overlay-image">');
+	var $title = $('<p id="overlay-title"></p>');
+	var $author = $('<p id="overlay-author"></p>');
+	var $year = $('<p id="overlay-year"></p>');
+	var $plot = $('<p id="overlay-plot"></p>');
+	var $exit = $('<div id="exit"><img src="img/close-button.svg" alt="exit"></div>');
+	var $prevArrow = $('<div id="prevArrow"><img src="img/left-arrow.svg" alt="previous" /></div>');
+	var $nextArrow = $('<div id="nextArrow"><img src="img/right-arrow.svg" alt="next" /></div>');
+
+	/* Function to open overlay */
+	function openOverlay(type) {
+		/* Add overlay to body of index.html */
+		$("body").append($overlay);
+		/* Stop click from opening img url */
+		event.preventDefault();
+
+		$overlay.append($image);
+		$overlay.append($title);
+		$overlay.append($author);
+		$overlay.append($year);
+		$overlay.append($plot);
+
+//		// If book is clicked...
+		if (type == book) {
+
+			// Append author name
+			$author.html('<p id="overlay-author">Author: ' + author + '</p>');
+//
+		} // end if statement for type = book
+
+		// If movie is clicked...
+		else if (type == movie) {
+			/* Clear book data */
+//			$overlay.hide($author);
+
+			/* get plot */
+		$plot.html('<p id="overlay-plot">Plot: ' + plot + '</p>');
+
+
+		} // end if statement for type = movie
+
+		// Append cover image
+		$image.attr('src', cover);
+
+		// Append title
+		$title.html('<p id="overlay-title">Title: ' + title + '</p>');
+
+		// Append year
+		$year.html('<p id="overlay-year">Year: ' + year + '</p>');
+
+		/* add exit button. */
+		$overlay.append($exit);
+
+		 /* call function to capture info for the clicked image */
+//		updateImage(cover, title);
+
+		/* add image to overlay */
+		$overlay.append($image);
+
+		/* add text captions to the images when viewed in the lightbox. */
+		$overlay.append(title);
+
+		/* add back and forward navigation buttons when lightbox is visible */
+		$image.after($prevArrow);
+		$image.before($nextArrow);
+
+		/* show the overlay */
+		$overlay.fadeIn(1500);
+	}; // end openOverlay()
+
+	/* When the next button is clicked... */
+	$nextArrow.on("click", function(event) {
+		nextImage();
+	});
+
+//	/* When right arrow key is pressed... */
+//	$("body").keydown(function(event){
+//		if ( event.which == 39 ) {
+//			nextImage();
+//	  }
+//	});
+//
+//	/* When the previous button is clicked... */
+//	$prevArrow.on("click", function(event){
+//		previousImage();
+//	});
+//
+//	/* When left arrow key is pressed... */
+//	$("body").keydown(function(event){
+//		if ( event.which == 37 ) {
+//			previousImage();
+//	  }
+//	});
+
+	/* Hide overlay when exit button is clicked. */
+	$exit.on("click", function() {
+		$overlay.fadeOut(1000).hide();
+	});
+
+	/* Hide overlay when esc key is pressed */
+	$("body").keydown(function(event) {
+		if (event.which == 27) {
+			$overlay.fadeOut(1000).hide();
+		}
+	});
+
+	/*** OVERLAY FUNCTIONS ***/
+//
+//	function updateImage(imageLocation, imageCaption) {
+//		/* update image source */
+//		$image.attr("src", imageLocation);
+//		/* set caption text */
+//		$caption.text(imageCaption);
+//	}
+//
+	function nextImage() {
+		 /* update index */
+		index++;
+		/* loop up to first image in gallery */
+		if (index >= $("#gallery li").length) {
+			index = 0;
+		}
+
+		title = movies[index].Title;
+
+		};
+
+//		/* use index to get next image */
+//		var nextImage = $("#gallery li").get(index).getElementsByTagName("a");
+//		/* get new image location and caption */
+//		var imageLocation = $(nextImage).attr("href");
+//		var imageCaption =  $(nextImage).children("img").attr("alt");
+//		/* update the overlay image */
+//		updateImage(imageLocation, imageCaption);
+//	}
+//
+//	function previousImage() {
+//		/* update the index */
+//		index--;
+//		/* loop back to last image in gallery */
+//		if (index < 0) {
+//			index = $("#gallery li").length - 1;
+//		}
+//		/* get the previous image by index */
+//		var prevImage = $("#gallery li").get(index).getElementsByTagName("a");
+//		/* update the image location and caption */
+//		var imageLocation = $(prevImage).attr("href");
+//		var imageCaption =  $(prevImage).children("img").attr("alt");
+//		/* update the overlay */
+//		updateImage(imageLocation, imageCaption);
+//	}
+	// end overlay code
